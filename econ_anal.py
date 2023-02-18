294c13a45d92d69e48796140a98951d383017447
David Chin <david.w.h.chin@gmail.com>
2023-02-18 18:30:33 -0500

    Colors

--- econ_anal.py
+++ econ_anal.py
@@ -11,7 +11,7 @@ debug_p = False
 
 plt.style.use('fivethirtyeight')
 #pd.set_option('max_columns', 500)
-#color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
+color_pal = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
 def read_api_key(filename):
     with open(filename, 'r') as f:
@@ -44,7 +44,7 @@ for myid in unemp_df.index:
     results = fred.get_series(myid)
     results = results.to_frame(name=myid)
     all_results.append(results)
-    time.sleep(0.1) # throttle requests to FRED
+    time.sleep(0.3) # throttle requests to FRED
 
 unemp_results = pd.concat(all_results, axis=1)
 
@@ -76,5 +76,59 @@ ax = unemp_states.loc[unemp_states.index == '2020-05-01'].T \
 ax.legend().remove()
 ax.set_xlabel('% Unemployed')
 plt.savefig('unemployment_2020-05.png', bbox_inches='tight')
+
+
+
+# participation rate
+part_df = fred.search('participation rate state', filter=('frequency', 'Monthly'))
+part_df = part_df.query('seasonal_adjustment == "Seasonally Adjusted" and units == "Percent"')
+
+part_id_to_state = part_df['title'].str.replace('Labor Force Participation Rate for ', '').to_dict()
+
+all_results = []
+for myid in part_df.index:
+    results = fred.get_series(myid)
+    results = results.to_frame(name=myid)
+    all_results.append(results)
+    time.sleep(0.3) # throttle requests to FRED
+
+part_states = pd.concat(all_results, axis=1)
+part_states.columns = [part_id_to_state[c] for c in part_states.columns]
+
+# fix DC
+unemp_states = unemp_states.rename(columns={'the District of Columbia': 'District of Columbia'})
+
+fig, axs = plt.subplots(10, 5, figsize=(30, 30), sharex=True)
+axs = axs.flatten()
+
+i = 0
+for state in unemp_states.columns:
+    if state in ["District of Columbia", "Puerto Rico"]:
+        continue
+
+    ax2 = axs[i].twinx()
+    unemp_states.query('index >= 2020 and index < 2022')[state] \
+        .plot(ax=axs[i], label='Unemployment')
+    part_states.query('index >= 2020 and index < 2022')[state] \
+        .plot(ax=ax2, label='Participation', color=color_pal[1])
+    ax2.grid(False)
+    axs[i].set_title(state)
+    i += 1
+
+plt.tight_layout()
+plt.savefig('unemp_participation.png', bbox_inches='tight')
+
+state_wanted = 'California'
+fig, ax = plt.subplots(figsize=(10, 5), sharex=True)
+ax2 = ax.twinx()
+unemp_states2 = unemp_states.asfreq('MS')
+line1 = unemp_states2.query('index >= 2020 and index < 2022')[state_wanted] \
+    .plot(ax=ax, label='Unemployment')
+line2 = part_states.dropna().query('index >= 2020 and index < 2022')[state_wanted] \
+    .plot(ax=ax2, label='Participation', color=color_pal[1])
+ax2.grid(False)
+ax.set_title(state_wanted)
+fig.legend(labels=['Unemployment', 'Participation'])
+plt.savefig('california.png', bbox_inches='tight')
 
 
